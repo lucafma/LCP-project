b86429612c366cde1b4bf21847d7e5f72050750f
Luca F. Marques <lucafmarqs@gmail.com>
2017-06-30 10:38:27 -0500

    SQL data for tests

---
+++ sql_fill.sql
@@ -0,0 +1,68 @@
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Vila Indaia", "Av. 18a", "585", "13506-715", -22.400346, -47.550888, 2, "Casa da Rola");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Vila Indaia", "Rua 11b", "1310", "13506-745", -22.398894, -47.549578, 3, "Casa do Luca");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Vila Nova", "Rua 13a", "329", "13506-745", -22.395714, -47.551081, 4, "Casa do Blat");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Claret", "Av. Três", "1450", "13503-251", -22.413986, -47.570849, 0, "Mc Donald's");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Estádio", "Rua Quatorze", "938", "13503-251", 22.414191, -47.569565, 1, "Burger King");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Cidade Jardim", "Rua 6", "182", "13501-070", 22.421477, -47.558565, 2, "Imobiliária Xavier Camargo");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Centro", "Rua 2", "1605", "", -22.404922, -47.560671, 3, "Sodiê Doces");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Centro", "Av. 12", "310", "13500-460", -22.404750, -47.561944, 4, "Torre da Pizza");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Centro", "Rua Cinco", "1791", "13500-180", -22.404126, -47.564064, 0, "Colégio Koelle");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Res. São José", "Rua 10", "244", "13505-507", -22.376671, -47.583339, 1, "Igreja Evangélica Aparecida Renascer Rio Claro");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Res. São José", "Av. M 51", "228", "13505-494", -22.374143, -47.578329, 4, "Kalango Lanches");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Panorama", "Rua 23", "399", "13504-656", -22.387462, -47.589895, 2, "Saibreja");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Vila Operária", "Rua Dr. Elói Chaves", "231", "13504-186", -22.391841, -47.569975, 3, "Prefeitura???");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Centro", "Av. 16", "1181", "13500-480", -22.405708, -47.571375, 4, "Unimed");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Centro", "Av. 5", "57", "13500-380", -22.411200, -47.558219, 0, "Fiorella");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Maria Cristina", "Av. 22 JC", "2", "13503-521", -22.416786, -47.590732, 1, "");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Res. Santa Eliza", "Av. 54 SE", "400", "13504-625", -22.395085, -47.592408, 2, "Agrovieira");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Vilage", "Rua 11 JV", "66", "13506-105", -22.377254, -47.547511, 3, "");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Distrito Industrial", "Av. Brasil", "4849", "", -22.360227, -47.552444, 4, "Tigre");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Bela Vista", "Av. 34", "1916", "13504-110", -22.398261, -47.578783, 0, "");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Panorama", "Rua 25", "40", "13504-658", -22.390550, -47.592112, 1, "Darcy Confecções");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Jardim Paulista II", "Rua 27", "3771", "13503-140", -22.393450, -47.589004, 2, "Bar da Tia Maria");
+
+INSERT INTO endereco (bairro, logradouro, numero, cep, lat, lng, tipo, descricao)
+values ("Parque Universitário", "Rua 19", "3425", "13504-363", -22.394612, -47.582839, 3, "Espaleiras Rio Claro");
